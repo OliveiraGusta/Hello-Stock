af2e1e90fd6201d21de56545682e6d2f20767655
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-02-11 00:43:13 -0300

    window.alert

--- login/valid_login.php
+++ login/valid_login.php
@@ -3,46 +3,44 @@
 //Verificar se a variavel entrar existe e Verificar se login e senha estiverem preenchidos
   if(isset($_POST['entrar']) && !empty($_POST['nome'])  && !empty($_POST['senha'])) 
   {
+    //Conexão com o Banco de Dados 
     include_once('../config.php');
 
     $nome = $_POST['nome'];
     $senha = $_POST['senha'];
 
-    
-    //print_r('<br>Nome: ' . $nome);
-    //print_r( '<br>Senha: ' .$senha);
-
     //Verificar se existe o nome e senha
     $sql = "SELECT * FROM cliente WHERE nome = '$nome' and senha = '$senha' ";
 
-    //result recebe connection da pagina config.php
+    //verifica recebe connection da pagina config.php
     $verifica = $connection->query($sql);
 
-    //print_r  ($result);
-    //print_r  ($sql);
-
     //Caso o numero de linhas do $verifica for menor que 1
-    if (mysqli_num_rows($verifica) < 1)
+    if(mysqli_num_rows($verifica) < 1)
     {
-      //Caso menor que 1, mostre o alerta
-
-      $alerta = "Nome e/ou senha incorretos";
-      echo "<script type='text/javascript'>alert('$alerta');</script>";
-
-      // LEMBRAR LEMBRAR
-      // LEMBRAR Criar sistema para voltar para essa tela  login.html
-      header('Location: login.html');
+      //Caso < que 1, mostre o alerta e volte para tela login.html
+      echo (
+        "<script language='JavaScript'>
+        window.alert('Nome e/ou senha incorretos')
+        window.location.href='login.html';
+        </script>");
     } 
     else
     {
+      //Caso igual ou maior que 1, entre na tela estoque.php
       header('Location: ../estoque/estoque.php');
     }
+
 
   }
 
   else
     {
-      header('Location: login.html');
+      echo (
+        "<script language='JavaScript'>
+        window.alert('Por favor, preencha todos os campos')
+        window.location.href='login.html';
+        </script>");
     }
 
 ?>
